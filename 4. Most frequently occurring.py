2ae118c82110c46b38138f43a4e0685220bc2cff
SayedFazal <syedfazal2956@gmail.com>
2025-02-10 09:12:54 +0530

    Update 4. Most frequently occurring.py

--- 4. Most frequently occurring.py
+++ 4. Most frequently occurring.py
@@ -1,8 +1,8 @@
 from collections import Counter
 def frequency_analysis(ciphertext):
-freq = Counter(ciphertext)
-sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-return sorted_freq
+    freq = Counter(ciphertext)
+    sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    return sorted_freq
 # Example usage
 ciphertext = "ZEBRAS ARE BEAUTIFUL ANIMALS"
 freq_list = frequency_analysis(ciphertext.replace(" ", ""))
